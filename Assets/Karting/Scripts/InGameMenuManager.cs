cabb80ddb7e2320c0d2c24e1d30c71ca5028d400
patrol220 <22000patrol@gmail.com>
2020-12-06 14:27:27 +0100

    feature for muting music

--- Assets/Karting/Scripts/InGameMenuManager.cs
+++ Assets/Karting/Scripts/InGameMenuManager.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class InGameMenuManager : MonoBehaviour
 {
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string BackgroundPref = "BackgroundPref";
+
     [Tooltip("Root GameObject of the menu used to toggle its activation")]
     public GameObject menuRoot;
     [Tooltip("Master volume when menu is open")]
@@ -13,8 +17,12 @@ public class InGameMenuManager : MonoBehaviour
     public Toggle shadowsToggle;
     [Tooltip("Toggle component for framerate display")]
     public Toggle framerateToggle;
+    [Tooltip("Toggle component for music control")]
+    public Toggle musicToggle;
     [Tooltip("GameObject for the controls")]
     public GameObject controlImage;
+
+    public AudioSource backgroundAudio;
 
     //PlayerInputHandler m_PlayerInputsHandler;
     FramerateCounter m_FramerateCounter;
@@ -23,6 +31,14 @@ public class InGameMenuManager : MonoBehaviour
     {
         //m_PlayerInputsHandler = FindObjectOfType<PlayerInputHandler>();
         //DebugUtility.HandleErrorIfNullFindObject<PlayerInputHandler, InGameMenuManager>(m_PlayerInputsHandler, this);
+
+        if (PlayerPrefs.GetInt(FirstPlay) == 0)
+        {
+            PlayerPrefs.SetInt(BackgroundPref, 1);
+            PlayerPrefs.SetInt(FirstPlay, -1);
+        }
+
+        UpdateSound();
 
         m_FramerateCounter = FindObjectOfType<FramerateCounter>();
         //DebugUtility.HandleErrorIfNullFindObject<FramerateCounter, InGameMenuManager>(m_FramerateCounter, this);
@@ -34,6 +50,9 @@ public class InGameMenuManager : MonoBehaviour
 
         framerateToggle.isOn = m_FramerateCounter.uiText.gameObject.activeSelf;
         framerateToggle.onValueChanged.AddListener(OnFramerateCounterChanged);
+
+        musicToggle.isOn = PlayerPrefs.GetInt(BackgroundPref) == 1 ? true : false;
+        musicToggle.onValueChanged.AddListener(OnMusicToggleChanged);
     }
 
     private void Update()
@@ -108,5 +127,30 @@ public class InGameMenuManager : MonoBehaviour
     public void OnShowControlButtonClicked(bool show)
     {
         controlImage.SetActive(show);
+    }
+
+    private void UpdateSound()
+    {
+        if (PlayerPrefs.GetInt(BackgroundPref) == 1)
+        {
+            backgroundAudio.volume = 1.0F;
+        } else
+        {
+            backgroundAudio.volume = 0.000F;
+        }
+    }
+
+    private void OnMusicToggleChanged(bool toggle)
+    {
+        if (toggle)
+        {
+            PlayerPrefs.SetInt(BackgroundPref, 1);
+        } else
+        {
+            PlayerPrefs.SetInt(BackgroundPref, 0);
+        }
+
+
+        UpdateSound();
     }
 }
